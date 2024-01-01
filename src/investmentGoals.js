e7a11896422fcdf32b60172823b80de54d949a09
duponce <eduardo@jponce.com.br>
2024-01-01 10:58:23 -0300

    Formulario de Investimento criado e arquivo de cálculo de projeções criado

---
+++ src/investmentGoals.js
@@ -0,0 +1,58 @@
+function convertToMonthlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  monthlyContribuiton = 0,
+  returnRate = 0,
+  timePeriod = "monthly",
+  returnTimeFrame = "monthly"
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      "Precisamos que passe os valores de Investimento Inicial e Prazo."
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returnRate / 100
+      : convertToMonthlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    InvestedAmount: startingAmount,
+    InterestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribuiton;
+    const InterestReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribuiton * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+    returnsArray.push({
+      InvestedAmount,
+      InterestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+
+  return returnsArray;
+}
